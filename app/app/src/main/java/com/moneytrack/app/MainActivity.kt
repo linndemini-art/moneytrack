5271d54fe140e9853b260aa714032b801c633464
linndemini-art <linnd.emini@gmail.com>
2026-09-20 18:25:59 +0200

    Update MainActivity.kt

--- app/app/src/main/java/com/moneytrack/app/MainActivity.kt
+++ app/app/src/main/java/com/moneytrack/app/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.moneytrack.app
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val textView = TextView(this).apply {
-            text = "MoneyTrack"
-            textSize = 28f
-            setPadding(40, 80, 40, 40)
-        }
+        val text = TextView(this)
+        text.text = "MoneyTrack is working"
+        text.textSize = 28f
 
-        setContentView(textView)
+        setContentView(text)
     }
 }
